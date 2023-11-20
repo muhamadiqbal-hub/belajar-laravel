daf1d15568daf06ca3d4fbafdbf725d36cc4dab5
muhamadiqbal-hub <ibhankgntng@gmail.com>
2023-11-19 20:47:42 -0800

    saya berhasil uploud project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <table>
+        <tr>
+            <th>no</th>
+            <th>nama</th>
+            <th>umur</th>
+            <th>keterangan</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>iqbal</td>
+            <td>19</td>
+            <td>kuliah</td>
+        </tr>
+    </table>
+    </center>
+</body>
+</html>
